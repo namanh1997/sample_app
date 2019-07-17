ccf7916663460198d475f1ce8cabc8d80974507a
namanh1997 <nguyennamnh2197@gmail.com>
2019-07-17 11:07:15 +0700

    fix fix fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-    before_action :set_locale
-    private
-    def set_locale
-        I18n.locale = params[:locale] || I18n.default_locale
-    end
+  before_action :set_locale
 
-    def default_locale
-        {locale: I18n.locale}
-    end
+  private
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_locale
+    {locale: I18n.locale}
+  end
+  
+  protect_from_forgery with: :exception
+
 end
